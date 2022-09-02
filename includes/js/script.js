fe5e4a4ab21a0a43fd85f16d1ce994f53a5d2f9e
Erica <ericaguadalupe511@gmail.com>
2022-09-01 20:34:48 -0400

    adds validation to index.html and forgotinfo.html

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,8 +1,28 @@
 // script.js 
 
 // creates popup notification letting user know email was sent 
-let button = document.getElementById('submitEmail');
+let button = document.querySelector("#submitEmail");
 
-button.addEventListener('click', function(){
-  console.log("clicked!");
-});
+// button.addEventListener('click', function(){
+//   alert("Sent email!");
+// });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
